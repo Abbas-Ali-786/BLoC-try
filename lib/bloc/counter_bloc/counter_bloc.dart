aa0e93cb5918c692dc7a65e59e080a7c7ab9f907
Abbas Ali <wwwabbasmughal786@gmail.com>
2024-06-08 10:59:55 +0500

    Update counter_bloc.dart

--- lib/bloc/counter_bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc/counter_bloc.dart
@@ -7,12 +7,10 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
     on<IncrementEvent>(_increment);
     on<DecrementEvent>(_decrement);
   }
-
-
+  
   void _increment(IncrementEvent event, Emitter<CounterState> emit){
     emit(state.copyWith(counter: state.counter + 1));
   }
-
   
   void _decrement(DecrementEvent event, Emitter<CounterState> emit){
     emit(state.copyWith(counter: state.counter - 1));
